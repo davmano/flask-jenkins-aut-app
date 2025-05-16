b0ffd052c7a76d458ee21866d1f60037ddf79392
David Mano <davmano51@gmail.com>
2025-05-16 07:51:33 +0000

    Add more resources

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
